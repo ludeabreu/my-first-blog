1812414e32c33b3e435bfed3270607d1cfbe716e
Luiza Correia <ludeabreu@gmail.com>
2024-10-19 15:47:57 -0300

    Modificação da view e template pra exibir a lista de posts na home do blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,18 @@
+""" Esse arquivo foi criado do zero para configurar os paths do blog.
+No urls.py geral (já existente na pasta mysite), precisaremos incluir (include) nos
+urlpatterns, um path para este arquivo blog>urls.py
+Normalmente, a função path recebe 3 argumentos:
+1. caminho acessado, 
+2. view que será executada ou include('aplicação.urls'), 
+3. nome opcional.
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+"""
+
+
 from django.urls import path
 from . import views
 
